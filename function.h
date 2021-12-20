96c19bd6a149878173eb8bd3d4e85bc25548aaa7
avidan.abitbol <avidan88@walla.com>
2021-12-20 21:18:04 +0200

    Update function.h

--- function.h
+++ function.h
@@ -1,1 +1,21 @@
+#ifndef EX3
+#define EX3
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include <conio.h>
+#include <string.h>
+#define TXT 1024
+#define WORD 30
 
+int CharToNum(char c)
+int WordCalc(char *c)
+void getText(char txt[TXT])
+void getWord(char word[WORD])
+char AtbChar(char c)
+void Gematry(int val,char t[])
+char AtbWord(char *c)
+void Atbash_Sequences(char ch[],char words[])
+void Anagram(char str[],char txt[])
+
+#endif
